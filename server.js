cded6a9303d5861e8c0390d14e714b0f37ed7f6e
ASim530700 <administracao@activosim.pt>
2025-07-08 00:45:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,68 +4,60 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middlewares
 app.use(bodyParser.json());
 app.use(cors());
 
-//mongoose
+// MongoDB connection
 mongoose.connect("mongodb+srv://neil:neil140301@cluster0.g5a3e.mongodb.net/emailDb");
 
-//data schema and model
+// Schema e modelo
 const templateSchema = {
   templateId: Number,
   templateJson: String
-}
+};
+const Template = mongoose.model("Template", templateSchema);
 
-const Template = mongoose.model("Template",templateSchema);
-
-//Api routes
-app.get('/templates', async (request,response) => {
+// API routes
+app.get('/templates', async (req, res) => {
   const results = await Template.find();
-  const templates = results.map(item => item.templateId)
-  response.send(templates);
+  const templates = results.map(item => item.templateId);
+  res.send(templates);
 });
 
-app.get('/template/:id', async (request,response) => {
-  const {id} = request.params;
-  const template = await Template.find({templateId: id});
-  response.send(template);
+app.get('/template/:id', async (req, res) => {
+  const { id } = req.params;
+  const template = await Template.find({ templateId: id });
+  res.send(template);
 });
 
-app.post('/template', (request, response) => {
-  const {templateId, templateJson} = request.body;
-  console.log('hi');
-  const newTemplate = new Template({templateId,templateJson});
-  newTemplate.save();
+app.post('/template', async (req, res) => {
+  const { templateId, templateJson } = req.body;
+  const newTemplate = new Template({ templateId, templateJson });
+  await newTemplate.save();
+  res.sendStatus(201);
 });
 
-app.put('/template/:id', async (request,response) => {
-  const {id} = request.params;
-  const {templateJson} = request.body;
-  console.log(templateJson)
-  const result = await Template.updateOne({templateId: id},{$set: {templateJson}})
-  response.send("updated");
+app.put('/template/:id', async (req, res) => {
+  const { id } = req.params;
+  const { templateJson } = req.body;
+  await Template.updateOne({ templateId: id }, { $set: { templateJson } });
+  res.send("updated");
 });
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('*', (request,response) => {
-    response.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+// Static frontend (React) build
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
-// server start
+// Start server
 app.listen(port, () => {
-  console.log("express is running");
+  console.log(`Express is running on port ${port}`);
 });
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
-});
-
